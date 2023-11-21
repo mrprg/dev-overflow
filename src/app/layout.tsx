9e326d388b3bf7b4596aad0e953349607813704a
mrprg <mohamadrezaasadi322@gmail.com>
2023-11-21 18:07:15 +0330

    add some fonts to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "Dev OverFlow",
@@ -23,7 +43,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body
+          className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">this is piece of text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
