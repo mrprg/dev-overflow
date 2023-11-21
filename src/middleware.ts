e65335cec1c8524630c23cc3b5d81cc501d6ad99
mrprg <mohamadrezaasadi322@gmail.com>
2023-11-21 16:56:03 +0330

    change directory of middleware for hancling protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+  // "/" will be accessible to all users
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
+};
