b8e54535f6f1f31647627c8fbfe4d6db851b3bdf
mrprg <mohamadrezaasadi322@gmail.com>
2023-11-28 19:35:54 +0330

    fix some stypo mistakes (i dont why i have so many of this mistakes LOL :/ )

--- src/components/shared/search/GlobalSearch.tsx
+++ src/components/shared/search/GlobalSearch.tsx
@@ -15,7 +15,7 @@ const GlobalSearch = () => {
         />
         <Input
           type="text"
-          placeholder="Search globally"
+          placeholder="Search for Questions Here..."
           value=""
           className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
